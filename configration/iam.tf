666087dd6eebebeeb46a9d59a9e627d113a93f09
mguler47 <mehmetgulerdevops@gmail.com>
2020-01-29 13:57:19 -0600

    iam

--- configration/iam.tf
+++ configration/iam.tf
@@ -9,9 +9,9 @@ resource "aws_iam_group" "developers" {
 resource "aws_iam_group_membership" "developers_team" { 
   name = "developers-group-membership" 
   users = [ 
-    aws_iam_user.tim.name 
-  ] 
-  group = aws_iam_group.developers.name   
+    aws_iam_user.tim.name
+    ]
+    group = aws_iam_group.developers.name   
 } 
 
 resource "aws_iam_policy" "policy" { 
