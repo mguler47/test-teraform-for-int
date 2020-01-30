45132d64c024cfe4bb01da681551b5d12dd972ac
mguler47 <mehmetgulerdevops@gmail.com>
2020-01-30 13:22:06 -0600

    added sg

---
+++ sg.tf
@@ -0,0 +1,35 @@
+esource "aws_security_group" "allow_ssh" { 
+  name        = "allow_ssh" 
+  description = "Allow TLS inbound traffic" 
+  vpc_id      = "vpc-a0c880da" 
+
+  ingress { 
+    from_port   = 22 
+    to_port     = 22 
+    protocol    = "tcp" 
+    cidr_blocks = ["0.0.0.0/0"]   
+
+} 
+ingress { 
+    from_port   = 443 
+    to_port     = 22 
+    protocol    = "tcp" 
+    cidr_blocks = ["0.0.0.0/0"] 
+    
+    } 
+    } 
+ingress { 
+    from_port   = 80 
+    to_port     = 22 
+    protocol    = "tcp" 
+    cidr_blocks = ["0.0.0.0/0"] 
+    
+    } 
+    } 
+ingress { 
+    from_port   = 8080 
+    to_port     = 22 
+    protocol    = "tcp" 
+    cidr_blocks = ["0.0.0.0/0"] 
+    
+    } 
