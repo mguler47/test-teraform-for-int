79ab68ca5e19e72c90e6f2f8c15ab7cc30ee7a32
mguler47 <mehmetgulerdevops@gmail.com>
2020-01-30 15:28:56 -0600

    addedvpc

--- configration/vpc.tf
+++ configration/vpc.tf
@@ -1,23 +1,14 @@
-resource "aws_subnet" "public" {
-  vpc_id     = "${aws_vpc.dev.id}"
-  cidr_block = "10.0.1.0/24"
-
+module "wordpress1" {
+  source = "terraform-aws-modules/vpc/aws"
+  name = var.name
+  cidr = var.cidr
+  azs             = [var.azs1, var.azs2, var.azs3]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
   tags = {
-    Name = "${var.Name}.public"
-    Env = "${var.Env}"
-    Created_by = "${var.Created_by}"
-    Dept = "${var.Dept}"
-  }
-}
-
-resource "aws_subnet" "private" {
-  vpc_id     = "${aws_vpc.dev.id}"
-  cidr_block = "10.0.2.0/24"
-
-  tags = {
-    Name = "${var.Name}.private"
-    Env = "${var.Env}"
-    Created_by = "${var.Created_by}"
-    Dept = "${var.Dept}"
+    Terraform = "true"
+    Environment = "dev"
   }
 }
