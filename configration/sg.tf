91bb5c244d4696a73448157e04f5be3ef71aaa71
mguler47 <mehmetgulerdevops@gmail.com>
2020-01-30 13:38:31 -0600

    edit

--- configration/sg.tf
+++ configration/sg.tf
@@ -4,32 +4,29 @@ esource "aws_security_group" "allow_ssh" {
   vpc_id      = "vpc-a0c880da" 
 
   ingress { 
-    from_port   = 22 
-    to_port     = 22 
-    protocol    = "tcp" 
-    cidr_blocks = ["0.0.0.0/0"]   
+      from_port   = 22 
+      to_port     = 22 
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"] 
+}
 
-} 
   ingress { 
-    from_port   = 443 
-    to_port     = 22 
-    protocol    = "tcp" 
-    cidr_blocks = ["0.0.0.0/0"] 
-    
+      from_port   = 443 
+      to_port     = 22 
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]   
+
 }
   ingress { 
-    from_port   = 80 
-    to_port     = 22 
-    protocol    = "tcp" 
-    cidr_blocks = ["0.0.0.0/0"] 
-    
+      from_port   = 80 
+      to_port     = 22 
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"] 
 }
-  ingress { 
-    from_port   = 8080 
-    to_port     = 22 
-    protocol    = "tcp" 
-    cidr_blocks = ["0.0.0.0/0"] 
 
-    
-    
-} 
+  ingress { 
+      from_port   = 8080 
+      to_port     = 22 
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"] 
+}
