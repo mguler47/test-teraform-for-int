a0a667b39704280a23644e78395d59d20bc9c82b
mguler47 <mehmetgulerdevops@gmail.com>
2020-01-30 13:34:37 -0600

    aded

--- configration/sg.tf
+++ configration/sg.tf
@@ -10,26 +10,26 @@ esource "aws_security_group" "allow_ssh" {
     cidr_blocks = ["0.0.0.0/0"]   
 
 } 
-ingress { 
+  ingress { 
     from_port   = 443 
     to_port     = 22 
     protocol    = "tcp" 
     cidr_blocks = ["0.0.0.0/0"] 
     
-} 
-      
-ingress { 
+}
+  ingress { 
     from_port   = 80 
     to_port     = 22 
     protocol    = "tcp" 
     cidr_blocks = ["0.0.0.0/0"] 
     
-} 
-    
-ingress { 
+}
+  ingress { 
     from_port   = 8080 
     to_port     = 22 
     protocol    = "tcp" 
     cidr_blocks = ["0.0.0.0/0"] 
+
+    
     
 } 
